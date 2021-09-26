9f5cf37dda70c12d0690c5c07ca6fbd057a9d7bb
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2021-09-26 21:45:44 +0530

    Starter Files Added

--- Day2/Tip-Calculator.py
+++ Day2/Tip-Calculator.py
@@ -5,7 +5,7 @@ tip_percent = int(input("What percentage tip would you like to give ? 10, 12 ,or
 total_bill = bill * (1 + (tip_percent/100))
             # bill * (1 + 0.12) => bill * 1.12
 no_of_people = int(input("How many people to split the bill ?"))
-each_pay = round(total_bill/no_of_people,2)
+each_pay = "{:.2f}".format(total_bill/no_of_people,2)
 
 print(f"Each Person should pay : ${each_pay}")
 
